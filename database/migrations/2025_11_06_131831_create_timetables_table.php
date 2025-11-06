cb2f12f4106c292c2ffe4645d67faf71150777d4
tgrsnts <tegarsantoso72@gmail.com>
2025-11-06 14:34:47 +0700

    add migration and seeder

---
+++ database/migrations/2025_11_06_131831_create_timetables_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('timetables', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('coach_id');
+            $table->date('date');
+            $table->time('start_time');
+            $table->time('end_time');
+            $table->string('level');
+            $table->string('price');
+            $table->string('max_slots');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('timetables');
+    }
+};
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Coach;
+use App\Models\Timetable;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -15,9 +17,50 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        // User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+
+      User::create([
+            "name" => "Tegar Santoso",
+            "email" => "admin@gmail.com",
+            "password" => "rahasia",
+            // "role" => 1,
+            // "jenis_kelamin" => "L",
+            // "telepon" => "+62879076582341"
+        ]);
+
+        Coach::create([
+            'name' => 'Achmad Fauzal',
+            'specialty' => 'Beginner Coaching',
+            'photo_url' => 'https://example.com/photos/john_doe.jpg',
+        ]);
+
+        Coach::create([
+            'name' => 'Ferizwan',
+            'specialty' => 'Beginner Coaching',
+            'photo_url' => 'https://example.com/photos/john_doe.jpg',
+        ]);
+
+        Timetable::create([
+            'coach_id' => 1,
+            'date' => '2023-12-01',
+            'start_time' => '10:00:00',
+            'end_time' => '11:00:00',
+            'level' => 'Beginner',
+            'price' => '100000',
+            'max_slots' => '5',
+        ]);
+
+        Timetable::create([
+            'coach_id' => 2,
+            'date' => '2023-12-04',
+            'start_time' => '10:00:00',
+            'end_time' => '11:00:00',
+            'level' => 'Beginner',
+            'price' => '100000',
+            'max_slots' => '5',
         ]);
     }
 }
