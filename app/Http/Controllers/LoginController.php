b2d70c605a0026974779c326ab66494f910fbc41
tgrsnts <tegarsantoso72@gmail.com>
2025-11-27 13:01:40 +0700

    ubah redirect login

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -8,20 +8,26 @@ use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
-    public function login(Request $request){
+    public function login(Request $request)
+    {
         $user = $request->validate([
             'email' => ['required', 'email'],
             'password' => 'required'
         ]);
 
-        if (Auth::attempt($user)){
+        if (Auth::attempt($user)) {
             $request->session()->regenerate();
-            return redirect()->intended('/booking');
+            if (Auth::user()->role == 'admin') {
+                return redirect()->intended('/admin');
+            } else {
+                return redirect()->intended('/booking');
+            }
         }
         return back()->with('email', 'Email atau Password Salah')->onlyInput('email');
     }
 
-    public function logout(Request $request){
+    public function logout(Request $request)
+    {
         Auth::logout();
         $request->session()->invalidate();
         $request->session()->regenerateToken();
